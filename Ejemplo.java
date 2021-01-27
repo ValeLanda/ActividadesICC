0965a34b9631e660f7272aa37560e853c7b68a38
ValeLanda <valeriaa.landaa@ciencias.unam.com>
2021-01-27 16:03:30 -0600

    Primer commit

---
+++ Ejemplo.java
@@ -0,0 +1,20 @@
+/**
+ *Clase para ejemplificar uso de GitHub
+ *@author Valeria García Landa
+ */
+
+public class Ejemplo{
+    public static void main(String args[]){
+	//Linea que modificará integrante 1
+	String nombre1= "";
+	
+	//Linea que modificará integrante 2
+	String nombre2= "";
+
+	//Linea que modificará integrante 1
+	System.out.println("Hola soy "+nombre1);
+
+	//Linea que modificará integrante 2
+	System.out.println("Hola soy "+nombre2);
+    }
+}
